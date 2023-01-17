890510cce798c81b3327085b1f3f789ec969ab84
DareFox <easymalink@gmail.com>
2023-01-17 21:09:48 +0300

    Add tests for argument parser

---
+++ src/commonTest/kotlin/ParseArgumentsTest.kt
@@ -0,0 +1,49 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFails
+
+class ParseArgumentsTest {
+    @Test
+    fun testParseArguments() {
+        assertFails { parseArguments(arrayOf("--text")) }
+        assertFails { parseArguments(arrayOf()) }
+        assertFails { parseArguments(arrayOf("--text", "smth", "--border")) }
+        assertFails { parseArguments(arrayOf("--text", "smth", "--border", "{")) }
+        assertFails { parseArguments(arrayOf("--text", "smth", "--border", "{}}")) }
+        assertFails { parseArguments(arrayOf("--text", "smth", "--borderLeft", "{}")) }
+        assertFails { parseArguments(arrayOf("--text", "smth", "--borderRight", "{}")) }
+        assertFails { parseArguments(arrayOf("--text", "smth", "--borderRight", "{", "--separator")) }
+        assertFails { parseArguments(arrayOf("--text", "smth", "--borderRight")) }
+        assertFails { parseArguments(arrayOf("--text", "smth", "--borderRight", "--borderLeft", "[")) }
+
+        parseArguments(arrayOf("--text", "smth", "--border", "{}"))
+        parseArguments(arrayOf("--text", "smth", "--borderLeft", "{"))
+        parseArguments(arrayOf("--text", "smth", "--borderRight", "}"))
+
+        val mixed = parseArguments(arrayOf(
+            "--text",
+            "smth",
+            "--borderRight",
+            "}",
+            "--border",
+            "[]",
+            "--borderLeft",
+            "{",
+            "--separator",
+            "!"
+        ))
+
+        assertEquals(
+            ']',
+            mixed.rightBoundSymbol
+        )
+        assertEquals(
+            '{',
+            mixed.leftBoundSymbol
+        )
+        assertEquals(
+            '!'.toString(),
+            mixed.separatorSymbol
+        )
+    }
+}
