3787d743f8068c845a24abf0b160b513049d2858
DareFox <easymalink@gmail.com>
2023-01-17 17:05:49 +0300

    Create function to "unfold" template and get all possible unique text variations

--- src/commonMain/kotlin/Tokenaizer.kt
+++ src/commonMain/kotlin/Tokenaizer.kt
@@ -1,4 +1,4 @@
-fun borderErrorCheck(arguments: Arguments) {
+private fun borderErrorCheck(arguments: Arguments) {
     // stack of indexes of opened brackets
     val stack = mutableListOf<Int>()
 
@@ -20,4 +20,58 @@ fun borderErrorCheck(arguments: Arguments) {
         val last = stack.last()
         "Not closed border at index $last\n\n" + underlineString(last, arguments.text)
     }
+}
+
+fun unfoldTemplate(arguments: Arguments): Set<String> {
+    borderErrorCheck(arguments)
+
+    val borderLeft = regexEscapeIfNecessary(arguments.leftBoundSymbol)
+    val borderRight = regexEscapeIfNecessary(arguments.rightBoundSymbol)
+    val regex = "$borderLeft(?!.*$borderLeft).*?$borderRight".toRegex()
+    var textVariations = setOf<String>(arguments.text)
+
+    while (true) {
+        val newTextVariations = mutableSetOf<String>()
+        var foundAnyTemplate = false
+
+        textVariations.forEach { text ->
+            val template = regex.find(text) ?: return@forEach
+            if (template != null) foundAnyTemplate = true
+
+            newTextVariations += getTextVariations(template, text, arguments)
+        }
+
+        if (!foundAnyTemplate) {
+            break
+        }
+
+        textVariations = newTextVariations
+    }
+
+    return textVariations
+}
+
+private fun getTextVariations(matchResult: MatchResult, text: String, arguments: Arguments): Set<String> {
+    val isEmpty = (matchResult.range.last - matchResult.range.first) == 1
+
+    if (isEmpty) {
+        return setOf(text.replaceRange(matchResult.range, ""))
+    }
+
+    val rangeWithoutBorders = matchResult.range.first + 1 until matchResult.range.last;
+    val values = text.substring(rangeWithoutBorders).split(arguments.separatorSymbol)
+
+    val textVariations = values.map {
+        text.replaceRange(matchResult.range, it)
+    }.toSet()
+
+    return textVariations
+}
+
+private fun regexEscapeIfNecessary(char: Char): String {
+    val isLetter = "\\w".toRegex().find(char.toString()) != null
+
+    return if (isLetter) {
+        char.toString()
+    } else "\\${char}"
 }
