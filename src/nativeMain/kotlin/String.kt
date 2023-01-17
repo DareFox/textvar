f341e676daf5fc8bcf6f5ed018d2a769d97c12be
DareFox <easymalink@gmail.com>
2023-01-17 03:47:44 +0300

    Create funciton to underline range in string

---
+++ src/nativeMain/kotlin/String.kt
@@ -0,0 +1,26 @@
+import kotlin.math.max
+import kotlin.math.min
+
+fun underlineString(range: IntRange, text: String): String {
+    require(range.last <= text.lastIndex) {
+        "Underline (${range.first}..${range.last}) is out of range (0..${text.lastIndex})."
+    }
+
+    val cropRange = max(range.first - 5, 0)..min(range.last + 5, text.lastIndex)
+    val crop = text.substring(cropRange)
+    var underline = ""
+
+    crop.forEachIndexed { index, _ ->
+        if (index in range) {
+            underline += "^"
+        } else {
+            underline += " "
+        }
+    }
+
+    return crop + "\n" + underline
+}
+
+fun underlineString(index: Int, text: String): String {
+    return underlineString(index..index, text)
+}
